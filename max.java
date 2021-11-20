90a35817c00f066a7ba830f2ee715834dea2996a
pasumarthivenky123 <93004329+pasumarthivenky123@users.noreply.github.com>
2021-11-20 17:09:50 +0530

    Update max.java

--- max.java
+++ max.java
@@ -8,17 +8,17 @@ public class max {
 	}
 	public static void Max(int x,int y)
 	{
-         int   Max=((x+y)-(x-y))/2;
+         int   Max=((x+y)+(x-y))/2;
 				System.out.println(+Max);
 	}
 	public static void Max(int x,int y,int z)
 	{
-         int   Max=((x+y+z)-(x-y-z))/3;
+         int   Max=((x+y+z)+(x-y-z))/3;
 				System.out.println(+Max);
 	}
 	public static void Max(int x,int y,int z,int k)
 	{
-         int   Max=((x+y+z+k)-(x-y-z-k))/4;
+         int   Max=((x+y+z+k)+(x-y-z-k))/4;
 				System.out.println(+Max);
 	}
 }
